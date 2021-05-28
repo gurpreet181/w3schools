d670654a98b37e821f15a1d9711bb75b9393057b
gurpreet181 <76861562+gurpreet181@users.noreply.github.com>
2021-05-28 09:38:27 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <html>
-<head><link rel="icon" href="https://drive.google.com/uc?export=view&id=1zWPDMDOm5SrBh_zHKCL1T93qBtyYDb7i">
+<head>
+<meta name="viewport" 
+content="width=device-width,initial-scale=1.0">
+<link rel="icon" href="https://drive.google.com/uc?export=view&id=1zWPDMDOm5SrBh_zHKCL1T93qBtyYDb7i">
 <body>
 
 <?php include_once 'new.html';?>
