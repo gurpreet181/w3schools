130207a564c4d330ee25cbbeb00366f30d03beaa
gurpreet181 <76861562+gurpreet181@users.noreply.github.com>
2021-06-01 08:53:47 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,9 @@
 <meta name="viewport" 
 content="width=device-width,initial-scale=1.0">
 <link rel="icon" href="https://drive.google.com/uc?export=view&id=1zWPDMDOm5SrBh_zHKCL1T93qBtyYDb7i">
+<link rel="stylesheet"
+      href="//cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.0.0/build/styles/default.min.css">
+<script src="//cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.0.0/build/highlight.min.js"></script>
 <body>
 
 <?php include_once 'new.html';?>
